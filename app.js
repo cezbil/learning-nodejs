4f284b3023d7e911fa90a71c38526416c5ce0414
cezbil <czaro93pl@gmail.com>
2019-11-20 23:29:06 +0000

    nd

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Hello World!</title></head>');
-    res.write('<body><h1>Hello World! heading</h1></body>');
-    res.write('</html>');
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
